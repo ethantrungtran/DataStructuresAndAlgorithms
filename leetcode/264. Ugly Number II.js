961d098f6ed231003b53419dc7e3bfaf0adef126
Dinh Trung <trantrung140219@gmail.com>
2024-08-18 22:17:07 -0500

    leetcode/264. Ugly Number II.js

---
+++ leetcode/264. Ugly Number II.js
@@ -0,0 +1,13 @@
+function nthUglyNumber(n) {
+    // Time complexity: O(n)
+    const dp = [];
+    dp[0] = 1;
+    let i2 = 0, i3 = 0, i5 = 0;
+    for (let i = 1; i < n; i++) {
+        dp[i] = Math.min(dp[i2] * 2, dp[i3] * 3, dp[i5] * 5);
+        if (dp[i] == dp[i2] * 2) i2++;
+        if (dp[i] == dp[i3] * 3) i3++;
+        if (dp[i] == dp[i5] * 5) i5++;
+    }
+    return dp[n-1];
+};
